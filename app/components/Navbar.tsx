69aa700dbd8fe07fac56270df0ee8336d0ca8986
Theo <theoslater1@gmail.com>
2024-05-05 20:34:26 +0100

    port over to new project

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import {
+  Box,
+  Button,
+  ButtonProps,
+  IconButton,
+  Stack,
+  styled,
+} from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import MailIcon from "@mui/icons-material/Mail";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+const ColorButton = styled(Button)<ButtonProps>(() => ({
+  color: "white",
+  backgroundColor: "rgb(48, 48, 48)",
+  "&:hover": {
+    backgroundColor: "rgb(44, 44, 44)",
+  },
+}));
+
+export default function Navbar() {
+  return (
+    <Box
+      sx={{
+        bgcolor: "rgb(34, 34, 34)",
+        borderColor: "rgb(55, 55, 55)",
+        borderWidth: "1px",
+        width: "100%",
+        height: "60px",
+        borderRadius: "14px",
+        marginBottom: "15px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "5px",
+      }}
+    >
+      <Stack spacing={1} direction="row" sx={{ flexGrow: 1 }}>
+        <ColorButton
+          style={{ height: "48px", width: "100px", borderRadius: "12px" }}
+        >
+          Home
+        </ColorButton>
+        <ColorButton
+          style={{ height: "48px", width: "100px", borderRadius: "12px" }}
+        >
+          Projects
+        </ColorButton>
+        <ColorButton
+          style={{ height: "48px", width: "100px", borderRadius: "12px" }}
+        >
+          Contact
+        </ColorButton>
+      </Stack>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          height: "48px",
+          bgcolor: "rgb(48,48,48)",
+          borderRadius: "15px",
+        }}
+      >
+        <Stack spacing={0} direction="row">
+          <IconButton
+            style={{ color: "white" }}
+            onClick={() =>
+              window.open("https://github.com/TheoSlater", "_blank")
+            }
+          >
+            <GitHubIcon />
+          </IconButton>
+          <IconButton style={{ color: "white" }}>
+            <LinkedInIcon></LinkedInIcon>
+          </IconButton>
+          <IconButton style={{ color: "white" }}>
+            <MailIcon></MailIcon>
+          </IconButton>
+        </Stack>
+      </Box>
+    </Box>
+  );
+}
