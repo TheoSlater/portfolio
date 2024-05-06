9900ce6d9e4dd0b0d7948cc2fe12b4414d5b296f
Theo Slater <theoslater1@gmail.com>
2024-05-06 20:07:07 +0100

    new page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,7 @@
-"use client";
-
-import { Container, Box, Avatar } from "@mui/material";
+import { Container, Box, Stack, Paper } from "@mui/material";
 import Navbar from "../components/Navbar";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function Projects() {
   return (
@@ -23,11 +22,52 @@ export default function Projects() {
             borderColor: "rgb(55, 55, 55)",
             borderWidth: "1px",
             width: "100%",
-            minHeight: "32vh",
+            minHeight: "65vh",
             borderRadius: "15px",
             margin: "auto",
           }}
-        ></Box>
+        >
+          <h1 style={{ fontSize: "50px", fontWeight: "bold" }}>Projects.</h1>
+          <p
+            style={{
+              fontSize: "20px",
+              marginTop: "10px",
+              marginBottom: "20px",
+            }}
+          >
+            I like to solve problems and build elegant solutions in my free
+            time. Here is some of the open source stuff I&apos;m working/worked
+            on.
+          </p>
+          <Stack spacing={5} direction="row">
+            <Box
+              sx={{
+                bgcolor: "rgb(48,48,48)",
+                width: "30%",
+                height: "50vh",
+                borderColor: "rgb(55,55,55)",
+                borderWidth: "1px",
+                borderRadius: "15px",
+                padding: "20px",
+              }}
+            >
+              PLACEHOLDER PROJECT NAME
+              <Box>
+                <Image
+                  src={"/pfp1.png"}
+                  alt={"PLACEHOLDER"}
+                  width={1000}
+                  height={1000}
+                  style={{
+                    borderColor: "rgb(55,55,55)",
+                    borderWidth: "1px",
+                    borderRadius: "20px",
+                  }}
+                ></Image>
+              </Box>
+            </Box>
+          </Stack>
+        </Box>
       </Container>
     </div>
   );
