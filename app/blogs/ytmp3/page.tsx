0fbb0a643254e1b968dc2d0f167e3e6fe9248536
Theo <theoslater1@gmail.com>
2024-05-10 21:30:13 +0100

    &apos;

--- app/blogs/ytmp3/page.tsx
+++ app/blogs/ytmp3/page.tsx
@@ -102,14 +102,15 @@ export default function Ytmp3() {
             }}
           >
             <p>
-              Let's rewind time to the starting month of January 2024 when I was
-              facing a problem continually during school hours—blocked YouTube
-              converter sites. Being fed up with overcoming this same recurring
-              difficulty, I quite deliberately started working on a solution.
-              The idea of making an application to convert YouTube to MP3 or MP4
-              in Python sparked a creative fire in me. When the evenings I came
-              back home were both tiresome and mentally draining, I made it a
-              habit to analyze the best moves that I could make. <br /> <br />
+              Let&apos;s rewind time to the starting month of January 2024 when
+              I was facing a problem continually during school hours—blocked
+              YouTube converter sites. Being fed up with overcoming this same
+              recurring difficulty, I quite deliberately started working on a
+              solution. The idea of making an application to convert YouTube to
+              MP3 or MP4 in Python sparked a creative fire in me. When the
+              evenings I came back home were both tiresome and mentally
+              draining, I made it a habit to analyze the best moves that I could
+              make. <br /> <br />
               The Python which was taught to me was taken on as a raw material
               and I never showed a lack of motivation. My goal was clear: to
               write an elegantly functional and user-centred code that strives
@@ -117,19 +118,20 @@ export default function Ytmp3() {
               intrusive ads, if any. The app launching turned out to be a
               crucial step not only in the journey to overcome a problem but
               also in my first chance to grasp proven computer science
-              technology skills and a chance to be original. <br /> <br /> At
-              this point, as I look back on the path, I am fully aware of the
-              difference that was made in my life by my choice to meet that
-              challenge on equal terms. One day from January 2024 I have been
-              committed to bringing my app to perfection through refining and
-              polishing. While designing the app, keeping the user convenience
-              in mind is of course the ultimate goal of the application and with
-              each iteration, I will make sure that users can convert their
-              favorite YouTube videos without any difficulties. This project, by
-              far, has gone beyond just being a solution to my problem. Instead,
-              it has become a reflection of how resilient and driven I can be.
-              Yet, I know accompanying the next chapter of what lies ahead is
-              full of new possibilities and thrilling.
+              technology skills and a chance to be original. <br></br>
+              <br /> At this point, as I look back on the path, I am fully aware
+              of the difference that was made in my life by my choice to meet
+              that challenge on equal terms. One day from January 2024 I have
+              been committed to bringing my app to perfection through refining
+              and polishing. While designing the app, keeping the user
+              convenience in mind is of course the ultimate goal of the
+              application and with each iteration, I will make sure that users
+              can convert their favorite YouTube videos without any
+              difficulties. This project, by far, has gone beyond just being a
+              solution to my problem. Instead, it has become a reflection of how
+              resilient and driven I can be. Yet, I know accompanying the next
+              chapter of what lies ahead is full of new possibilities and
+              thrilling.
             </p>
           </Box>
           <Box
@@ -162,8 +164,9 @@ export default function Ytmp3() {
           >
             <p style={{ marginBottom: "15px" }}>
               Using Tkinter for Python allowed me to create some simple UI/UX
-              for the buttons and text box. Although Tkinter isn't as rewarding
-              in terms of good-looking UI it still does the job. <br /> <br />
+              for the buttons and text box. Although Tkinter isn&apos;t as
+              rewarding in terms of good-looking UI it still does the job.{" "}
+              <br /> <br />
               After messing around with tk for a bit I created a simple window
               with a text box and a button that allowed me to move further into
               the backend of the program.
