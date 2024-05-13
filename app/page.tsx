6af010e80006c4aaef97550810b63057f0b869d5
Theo <theoslater1@gmail.com>
2024-05-13 19:55:43 +0100

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -60,16 +60,15 @@ export default function Home() {
             style={{ paddingLeft: 0, paddingRight: 0, marginTop: "35px" }}
           >
             <p style={{ fontSize: "20px" }}>
-              I&apos;m a junior programmer in England where I go to school.
-              Although I haven&apos;t had any formal work experience which in
-              the area of programming, I have developed an interest in front-end
-              development, building apps and websites. I have a passion with
-              experimenting and developing new ideas for websites or apps. While
-              doing my studies, my times will also be available for freelance
-              work. If you are looking for someone to cooperate with on projects
-              related to the digital space, I will be glad to share how we can
-              join forces. If you want to team up with me, I&apos;m all ears, so
-              feel free to,{" "}
+              I&apos;m a junior programmer in England. Although I haven&apos;t
+              had any formal work experience which in the area of programming, I
+              have developed an interest in front-end development, building apps
+              and websites. I have a passion with experimenting and developing
+              new ideas for websites or apps. While doing my studies, my times
+              will also be available for freelance work. If you are looking for
+              someone to cooperate with on projects related to the digital
+              space, I will be glad to share how we can join forces. If you want
+              to team up with me, I&apos;m all ears, so feel free to,{" "}
               <Link
                 href="/contact"
                 style={{ textDecoration: "underline", fontWeight: "bold" }}
