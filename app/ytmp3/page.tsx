ba3f8edef02d76f6d841758067eaeda11a348cdf
Theo <theoslater1@gmail.com>
2024-05-28 15:03:23 +0100

    page

--- app/ytmp3/page.tsx
+++ app/ytmp3/page.tsx
@@ -1,41 +1,54 @@
 "use client";
-
 import { useState } from "react";
 
 const YtMp3 = () => {
   const [url, setUrl] = useState("");
   const [response, setResponse] = useState<string | null>(null);
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleConvert = async () => {
     try {
-      const res = await fetch("http://localhost:5000/api/convert", {
+      const currentUrl = url;
+      const res = await fetch("https://ytmp3-server.vercel.app/api/convert", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ url }),
+        body: JSON.stringify({ url: currentUrl }),
       });
+      if (!res.ok) {
+        throw new Error("Conversion failed");
+      }
       const data = await res.json();
-      setResponse(data.message || data.error);
-    } catch (error) {
-      setResponse("Error: Could not reach the server.");
+      if ("error" in data) {
+        throw new Error(data.error);
+      }
+      const audioUrl = data.audio_file_path;
+      const link = document.createElement("a");
+      link.href = audioUrl;
+      link.setAttribute("download", "converted.mp3");
+      document.body.appendChild(link);
+      link.click();
+      if (link.parentNode) {
+        link.parentNode.removeChild(link);
+      }
+      setResponse("Conversion successful. File will be downloaded.");
+    } catch (error: any) {
+      console.error("Error converting:", error);
+      setResponse(error.message);
     }
   };
 
   return (
     <div>
       <h1>YouTube to MP3 Converter</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-          placeholder="Enter YouTube URL"
-        />
-        <button type="submit">Convert</button>
-      </form>
-      {response && <div>{response}</div>}
+      <input
+        type="text"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+        placeholder="Enter YouTube URL"
+      />
+      <button onClick={handleConvert}>Convert</button>
+      {response && <p>{response}</p>}
     </div>
   );
 };
